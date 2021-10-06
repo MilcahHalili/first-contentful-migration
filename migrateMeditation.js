7a12afb8f2fee24155411388b123021850e02e86
Milcah <code@milcah.dev>
2021-10-05 17:50:48 -0700

    remove unnecessary directories and files

--- migrateMeditation.js
+++ migrateMeditation.js
@@ -1,7 +1,7 @@
 const createMeditation = (migration, context) => {
-  const meditation = migration.createContentType('meditation')
-  const type = meditation.createField('type')
-  type.type('Symbol').required(true)
+  const meditation = migration.editContentType('meditation')
+    .name('Meditation')
+    .editField('type').type('Symbol').name('Type')
 }
 
 module.exports = createMeditation
